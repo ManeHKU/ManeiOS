83dbe1aad8dc507c5fa0a3362d84d2b9c2be364e
Thomas Yau <github@yaucp.dev>
2023-12-29 01:38:48 +0800

    init

---
+++ Mane HKU/Mane HKU/Mane_HKUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mane_HKUApp.swift
+//  Mane HKU
+//
+//  Created by Yau Chin Pang on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ManeHKUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
